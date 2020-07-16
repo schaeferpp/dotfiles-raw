7590532d804300232228bbb7d91230a924156e9c
Paul Schaefer <schaefer.pp@gmail.com>
2020-07-16 17:03:37 +0200

    bin updates

--- bin/resize.sh
+++ bin/resize.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-MAX_SIZE=250000
+MAX_SIZE=600000
 size=$(stat -c%s "$1")
-convert "$1" -strip -interlace Plane -gaussian-blur 0.05 -quality 85% "$1"
+convert "$1" -strip -interlace Plane -quality 80% "$1"
 while [ $size -ge $MAX_SIZE ] ; do
         echo converting $1
         convert "$1" -resize 90% "$1"
